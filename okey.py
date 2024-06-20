7b329ff75c96ad2204e5d2f42b04d4e0722bb10e
ztrackperu <ztrack@zgroup.com.pe>
2024-06-20 16:07:13 -0500

    base okey 2

--- okey.py
+++ okey.py
@@ -10,8 +10,16 @@ db = client.REPOSITORIO_6_2024
 
 collection = db.tunel
 
-query = collection.find_one()
+query = collection.find_one({},{"_id":-1})
 print(query)
 
 print("jale ok!")
 print(query['fecha'])
+#establecer proceso de datos 
+#solicitar id
+
+db1 = client.ZGTU0015_6_2024
+
+collection1 = db1.madurador
+
+query1 = collection1.find_one()
