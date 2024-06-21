fbd0078c2ca19ffbbaf646ad74576ac82c2e2235
ztrackperu <ztrack@zgroup.com.pe>
2024-06-21 14:37:48 -0500

    con mysql 10

--- tunel_mysql.py
+++ tunel_mysql.py
@@ -10,7 +10,9 @@ db_connection = mysql.connector.connect(
 # creating database_cursor to perform SQL operation
 db_cursor = db_connection.cursor()
 # executing cursor with execute method and pass SQL query
-db_cursor.execute("SELECT * FROM  contenedores WHERE nombre_contenedor='ZGTU0015'")
+#db_cursor.execute("SELECT * FROM  contenedores WHERE nombre_contenedor='ZGTU0015'")
+db_cursor.execute("SELECT * FROM  contenedores WHERE nombre_contenedor='ZGRU8747550'")
+
 #db_cursor.execute("SELECT * FROM  contenedores ")
 
 #db_cursor.execute("CREATE DATABASE my_first_db")
@@ -26,6 +28,9 @@ db_cursor.execute("SELECT * FROM  contenedores WHERE nombre_contenedor='ZGTU0015
 	
 #crear registro sino existe 
  #
+update_old_salary = (
+  "UPDATE salaries SET to_date = %s "
+  "WHERE emp_no = %s AND from_date = %s")
 
 print(db_cursor)
 
