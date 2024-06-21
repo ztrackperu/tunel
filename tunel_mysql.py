a568bc3325187fcc0cee6d61902209132725fecf
ztrackperu <ztrack@zgroup.com.pe>
2024-06-21 11:56:11 -0500

    con mysql 4

--- tunel_mysql.py
+++ tunel_mysql.py
@@ -10,9 +10,15 @@ db_connection = mysql.connector.connect(
 # creating database_cursor to perform SQL operation
 db_cursor = db_connection.cursor()
 # executing cursor with execute method and pass SQL query
-db_cursor.execute("SELECT * FROM  contenedores")
+db_cursor.execute("SELECT * FROM  contenedores WHERE nombre_contenedor='ZGTU0015'")
 #db_cursor.execute("CREATE DATABASE my_first_db")
 
+if db_cursor :
+	print("con resultado")
+else :
+	print("no pasaa nada ")
 #print all databases
 for db in db_cursor:
 	print(db)
+	
+#crear registro sino existe , 
