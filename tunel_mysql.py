b6afc3863b985492d8e40292acedecc80f4d6f09
ztrackperu <ztrack@zgroup.com.pe>
2024-06-21 11:42:08 -0500

    con mysql 3

--- tunel_mysql.py
+++ tunel_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_connection = tunel_mysql.connector.connect(
+db_connection = mysql.connector.connect(
     host= "localhost",
     user= "ztrack2023",
     passwd= "lpmp2018",
@@ -12,6 +12,7 @@ db_cursor = db_connection.cursor()
 # executing cursor with execute method and pass SQL query
 db_cursor.execute("SELECT * FROM  contenedores")
 #db_cursor.execute("CREATE DATABASE my_first_db")
+
 #print all databases
 for db in db_cursor:
 	print(db)
