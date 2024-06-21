9663c36ced34df8f91078226a705e7ce02ec59d4
ztrackperu <ztrack@zgroup.com.pe>
2024-06-21 15:19:34 -0500

    con mysql 15

--- tunel_mysql.py
+++ tunel_mysql.py
@@ -35,8 +35,27 @@ update_old_salary = (
 print(db_cursor)
 
 for db in db_cursor :
-    print("aqui va :")
+    #print("aqui va :")
     print(db[0])
-    print("termino de dato")
+    #print("termino de dato")
+    if db[0]==0 :
+        #crear nuevo registro
+        print("oli")
+        consulta_tunel =(
+            "INSERT INTO contenedores ( nombre_contenedor ,tipo, descripcionc, telemetria_id,set_point, temp_supply_1,temp_supply_2,return_air,evaporation_coil,"
+            "condensation_coil, compress_coil_1,compress_coil_2,ambient_air , cargo_1_temp ,cargo_2_temp,cargo_3_temp,cargo_4_temp,relative_humidity,avl , suction_pressure "
+            ",discharge_pressure,line_voltage, line_frequency,consumption_ph_1,consumption_ph_2 , consumption_ph_3 ,co2_reading,o2_reading,evaporator_speed,condenser_speed,"
+            "battery_voltage,power_kwh,power_trip_reading,   power_trip_duration, suction_temp,discharge_temp,supply_air_temp , return_air_temp ,dl_battery_temp,dl_battery_charge"
+            ",power_consumption,power_consumption_avg,alarm_present , capacity_load ,power_state,controlling_mode,humidity_control,humidity_set_point,fresh_air_ex_mode ,"
+            "fresh_air_ex_rate ,fresh_air_ex_delay,set_point_o2, set_point_co2,defrost_term_temp,defrost_interval , water_cooled_conde ,usda_trip,evaporator_exp_valve,"
+            "suction_mod_valve,hot_gas_valve,economizer_valve,ethylene , stateProcess,stateInyection, timerOfProcess,modelo,latitud , longitud ,ultima_fecha ,empresa_id,"
+            "defrost_prueba,ripener_prueba) VALUES ('ZGTU0015','Madurador','Tunel USA',1000000,"
+            "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,0,0,0,0,0,0,0,0,0,0,0,%s,"
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'THERMOKING,0,0,%s,1,0,0)"
+        )
+        db_cursor.execute(consulta_tunel,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,"2024-06-21 11:58:41")
 
+    else :
+        #actualizar el 
+        print("oliz")
 print("trizteza")
